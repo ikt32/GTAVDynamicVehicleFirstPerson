c8ff9e4bdfa51b1f7ef8c22f2e590df200812172
E66666666 <6639510+E66666666@users.noreply.github.com>
2023-01-07 17:41:18 +0100

    Delete more boilerplate

--- DynamicVehicleFirstPerson/Script.hpp
+++ DynamicVehicleFirstPerson/Script.hpp
@@ -2,17 +2,7 @@
 #include "FPVScript.hpp"
 #include "ScriptMenu.hpp"
 
-/*
- * Script.hpp exposes functions to initialize and manage the script.
- */
 namespace FPV {
-    // ScriptHookV calls this when starting the script.
     void ScriptMain();
-
-    /*
-     * BuildMenu builds the menu with its options and submenus.
-     * This function is declared here, so Script.cpp can call it
-     * The implementation is in MenuExampleMenu.cpp
-     */
     std::vector<CScriptMenu<CFPVScript>::CSubmenu> BuildMenu();
 }

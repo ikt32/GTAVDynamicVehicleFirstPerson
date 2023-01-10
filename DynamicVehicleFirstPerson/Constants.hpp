4ac9a2ff11e39a6bcc3959f8964e68a6f9c0466f
E66666666 <6639510+E66666666@users.noreply.github.com>
2023-01-10 23:40:41 +0100

    Add suffix possibility to version

--- DynamicVehicleFirstPerson/Constants.hpp
+++ DynamicVehicleFirstPerson/Constants.hpp
@@ -5,11 +5,12 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
+#define VERSION_SUFFIX ""
 
 namespace Constants {
     static const char* const ScriptName = "Dynamic Vehicle FPV";
     static const char* const NotificationPrefix = "~b~Dynamic Vehicle FPV~w~";
-    static const char* const DisplayVersion = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH);
+    static const char* const DisplayVersion = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH) VERSION_SUFFIX;
 
     static const char* const iktFolder = "ikt";
     static const char* const ScriptFolder = "DynamicVehicleFirstPerson";
